3b88760a53687746c7360c1ca3d3594da9ddb6fb
kelvin0601 <kelvin@bear.plus>
2025-12-03 18:01:45 +0700

    update

--- netlify/functions/fetchPolicy.js
+++ netlify/functions/fetchPolicy.js
@@ -8,11 +8,15 @@ export async function handler(event, context) {
         const response = await fetch(url);
         const html = await response.text();
 
+        const allowedOrigins = ['https://kestrel-labs.webflow.io', 'https://kestrellabs.webflow.io'];
+        const origin = event.headers.origin || event.headers.Origin;
+        const corsOrigin = allowedOrigins.includes(origin) ? origin : allowedOrigins[0];
+
         return {
             statusCode: 200,
             headers: {
                 'Content-Type': 'text/html',
-                'Access-Control-Allow-Origin': 'https://kestrel-labs.webflow.io, https://kestrellabs.webflow.io',
+                'Access-Control-Allow-Origin': corsOrigin,
                 'Access-Control-Allow-Methods': 'GET, OPTIONS',
                 'Access-Control-Allow-Headers': 'Content-Type',
             },
